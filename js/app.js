5d93eb0f806228743fce04db1039e57dc41723bc
Elliott Hughes-Taylor <ehughestaylor@hotmail.com>
2015-03-20 20:50:17 -0400

    new page

--- js/app.js
+++ js/app.js
@@ -1,23 +1,11 @@
-// $(window).load(function() {
-// 		// Animate loader off screen
-// 		$(".se-pre-con").fadeOut("slow");
-// });
-var fullWidth = function(){
-	$('#projects').on('click',function(){
-		$("div.project-left").removeClass('project-left').addClass('full-width');
-		$(".about-right").hide();
+var navFunc = function(){
+	$('.nav-trigger-inner').on('click',function(){
+		$('.nav-trigger').toggleClass('open');
+		$('.nav-full').slideToggle();
 	});
-};
+}
+
 
 $(document).ready(function(){
-	fullWidth();
-	$('#nav').on('mouseenter', function(){
-    	$('.dropdown-home').fadeIn();
-	})
-	$('.dropdown-home').on('mouseleave', function(){
-		$(this).fadeOut();
-	})
-	$('#nav').on('click', function(){
-    	$('.dropdown-home').fadeToggle();
-	})
+	navFunc();
 });
