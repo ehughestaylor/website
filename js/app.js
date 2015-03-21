89dbf80c29895cc912e716288468653b17ea1630
Elliott Hughes-Taylor <ehughestaylor@hotmail.com>
2015-03-21 13:32:22 -0400

    commit

--- js/app.js
+++ js/app.js
@@ -1,15 +1,20 @@
-$(window).load(function() {
-		// Animate loader off screen
-		// $(".se-pre-con").fadeOut("slow");
-});
+var navFunc = function(){
+	$('.nav-trigger-inner').on('click',function(){
+		$('.nav-trigger').toggleClass('open');
+		$('.nav-full').slideToggle();
+	});
+}
+
 
 $(document).ready(function(){
-	$('#nav').on('mouseenter', function(){
-    	$('.dropdown').fadeIn(function(){
-    		$('#nav').toggleClass("opaque");
-    	});
-	})
-	$('.dropdown').on('mouseleave', function(){
-		$(this).fadeOut();
-	})
+	navFunc();
+
+	var waypoint = new Waypoint({
+  		element: document.getElementById('section2'),
+  		handler: function() {
+    	 $('header').toggleClass('black');
+    	 $('.logo').toggleClass('logo-big').toggleClass('animated rotateIn');
+  }
+})
+
 });
