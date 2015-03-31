882e433775b3e39be11bec65254c7aabeba1da2c
Elliott Hughes-Taylor <ehughestaylor@hotmail.com>
2015-03-31 17:25:11 -0400

    new section

--- js/app.js
+++ js/app.js
@@ -4,26 +4,58 @@ var navFunc = function(){
 		$('.nav-full').slideToggle();
 	});
 }
-$(".port-button").click(function() {
+
+
+
+// var animatePoints = function(){
+//    var waypoint = new Waypoint({
+//         element: document.getElementById('section2'),
+//         handler: function() {
+//          $('header').toggleClass('clear');
+//          $('.logo').toggleClass('logo-big').toggleClass('animated rotateIn');
+//          $('.project-header').slideToggle();
+//          $('.line').toggleClass('black');
+//     }
+//   });
+// }
+
+
+
+$(document).ready(function(){
+   $('#fullpage').fullpage({
+      scrollOverflow: true
+   });
+	navFunc();
+  $('.bio-header').hide();
+  $('.project-header').hide();
+
+
+  $('.philly').hover(function(){
+    $('.philly').toggleClass('blue');
+    $('.philly-right').slideToggle();
+  })
+  $('.travel').hover(function(){
+    $(this).toggleClass('blue');
+    $('.traveling-left').fadeToggle();
+  })
+  $('.art').hover(function(){
+    $(this).toggleClass('blue');
+    $('.art-right').slideToggle();
+  })
+  $('.family').hover(function(){
+    $(this).toggleClass('blue');
+    $('.family-left').fadeToggle();
+  })
+  $('.me').hover(function(){
+    $(this).toggleClass('blue');
+    $('.me-middle').slideToggle();
+  })
+
+  $(".port-button").click(function() {
     $('html, body').animate({
         scrollTop: $("#section2").offset().top
     }, 1000);
     $('.nav-full').fadeOut();
-});
-
-
-$(document).ready(function(){
-	navFunc();
-	$('.project-header').hide();
-
-	var waypoint = new Waypoint({
-  		element: document.getElementById('section2'),
-  		handler: function() {
-    	 $('header').toggleClass('clear');
-    	 $('.logo').toggleClass('logo-big').toggleClass('animated rotateIn');
-    	 $('.project-header').slideToggle();
-    	 $('.line').toggleClass('black');
-  }
-})
+  });
 
 });
